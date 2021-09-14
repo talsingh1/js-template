3b5e8399127de9206502409c55003ce9794bc391
Talvinder <talvinder.singh@publicissapient.com>
2021-09-14 16:15:59 +0530

    vue setup

---
+++ src/js/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+  <div class="container">
+    <h1>Vue.js boilerplate</h1>
+    <p>FRONTEND RAMPUP</p>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      url: 'https://api.github.com/search/repositories',
+      events: []
+    }
+  },
+  created() {
+    axios.get(this.url, {
+      params: {
+        q: 'vuejs'
+      }
+    })
+    .then(response => this.events = response.data.items)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- webpack.extract.config.js
+++ webpack.extract.config.js
@@ -1,13 +1,14 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: {
-    'main': path.resolve(__dirname, './src/js/main.js')
+    'js/main': path.resolve(__dirname, './src/js/main.js')
   },
   output: {
-    path: path.resolve(__dirname, './dist/js'),
-    publicPath: '/dist/js/',
+    path: path.resolve(__dirname, './dist/'),
+    publicPath: '/dist/',
     filename: '[name].js'
   },
   module: {
@@ -19,8 +20,14 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            'scss': ExtractTextPlugin.extract({
+              fallback: 'vue-style-loader',
+              use: 'css-loader!sass-loader'
+            }),
+            'sass': ExtractTextPlugin.extract({
+              fallback: 'vue-style-loader',
+              use: 'css-loader!sass-loader?indentedSyntax'
+            })
           }
           // other vue-loader options go here
         }
@@ -36,9 +43,22 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'sass-loader']
+        })
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin({ filename: function(getPath){
+      return getPath('css/[name].css').replace('css/js', 'css');
+    }, disable: false, allChunks: true })
+  ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
@@ -64,7 +84,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: false
       }
